f541dd3acfbedfbee861788b7d5374a7c5f9f833
sayishuu <smile52673@gmail.com>
2024-01-29 19:51:23 +0900

    [feat] 순회 컨베이어밸트 구현
    순회하는 컨베이어밸트를
    test area 01에 구현하였다.
    밸트 방향에 따라 자동으로 진행방향이 결정된다.

---
+++ ressentiment/Assets/컨베이어벨트/beltMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beltMove : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // 현재 오브젝트의 전방 방향
+        Vector3 forwardDirection = transform.right;
+
+        Vector3 pos = rb.position;
+
+        // 벨트의 이동 벡터 계산 (전방 방향 기준)
+        rb.position += forwardDirection * speed * Time.fixedDeltaTime;
+
+        // 이동 적용
+        rb.MovePosition(pos);
+    }
+}
